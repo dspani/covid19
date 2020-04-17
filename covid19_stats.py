23ee7666590aa7fb2e890d396fd59721f523562f
Jaeha Choi <jaeha220@uw.edu>
2020-04-17 10:55:41 -0700

    Added countries.json

--- covid19_stats.py
+++ covid19_stats.py
@@ -2,5 +2,13 @@ import requests
 import json
 
 
-request = requests.get('https://api.covid19api.com/countries')
-print(request.text)
+# request = requests.get("https://api.covid19api.com/live/country/united-states/status/confirmed")
+# print(request.text)
+# jdata = json.dumps(request.text)
+
+fname = "by_country_example.json"
+
+with open("./json/"+fname) as jfile:
+    jdata = json.load(jfile)
+    for jobject in jdata:
+        print(str(jobject))
